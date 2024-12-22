5a05ded7c43ad5115a4314cb6c1d94479246d9df
MingYuanKee <mkee004@e.ntu.edu.sg>
2024-12-23 02:13:31 +0800

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+const app = require('express')();
+const moment = require('moment');
+const routes = require('./routes/routes');
+const onboardingTasks = require('./routes/onboardingTasks'); 
+
+app.use(bodyParse.json());
+
+app.use('/api', routes);
+
+// Live Reload configuration
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// Frontend route
+const FrontRouter = require('./routes/front');
+
+// Set ejs template engine
+app.set('view engine', 'ejs');
+
+app.use(connectLiveReload());
+
+app.use(bodyParse.urlencoded({ extended: false }));
+app.locals.moment = moment;
+
+// Database connection
+const db = require('./config/keys').mongoProdURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log(`Mongodb Connected`))
+    .catch(error => console.log(error));
+
+// Use the frontend route
+app.use(FrontRouter);
+
+// Use onboardingTasks route
+app.use(onboardingTasks);  
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
